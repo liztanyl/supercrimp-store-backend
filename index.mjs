86af891509cc979ecaa9b48dbc612eb952f88c49
liztanyl <elizabeth.tanyulin@gmail.com>
2022-05-24 20:55:12 +0800

    add cors dependency

--- index.mjs
+++ index.mjs
@@ -1,24 +1,25 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import methodOverride from "method-override";
-import dotenv from "dotenv";
+import cors from 'cors';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import methodOverride from 'method-override';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
 
 const app = express();
 app.use(
-  cors({
-    credentials: true,
-    origin: FRONTEND_URL,
-  })
+	cors({
+		credentials: true,
+		origin: FRONTEND_URL,
+	})
 );
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride("_method"));
-app.use(express.static("public"));
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
 app.use(express.json());
 
 const PORT = process.env.PORT || 3004;
