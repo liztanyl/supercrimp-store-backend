fa9674a47c57d740731e20e53524c38ec88ea3c8
gcskhor <gcskhor@yahoo.com.sg>
2022-05-24 13:58:03 +0800

    added controllers and routes

---
+++ controllers/colours.mjs
@@ -0,0 +1,36 @@
+export default function initColoursController(db) {
+  const allColours = async (request, response) => {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const availableColours = async (request, response) => {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const add = async (request, response) => {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const edit = async (request, response) => {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return {
+    allColours,
+    availableColours,
+    add,
+    edit,
+  };
+}
---
+++ controllers/orders.mjs
@@ -0,0 +1,20 @@
+export default function initOrdersController(db) {
+  const index = async (request, response) => {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const checkout = async (request, response) => {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return {
+    index,
+    checkout,
+  };
+}
