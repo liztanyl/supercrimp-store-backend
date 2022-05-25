c71de45658088de26993ad679dcfc8c79bfe9ec8
liztanyl <elizabeth.tanyulin@gmail.com>
2022-05-25 20:34:29 +0800

    edit products and colours controllers - change 'colourCode' to 'code' in products' colours map - add controller to get all available colours

--- controllers/colours.mjs
+++ controllers/colours.mjs
@@ -1,36 +1,45 @@
 export default function initColoursController(db) {
-  const allColours = async (request, response) => {
-    try {
-    } catch (error) {
-      console.log(error);
-    }
-  };
+	const allColours = async (request, response) => {
+		try {
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  const availableColours = async (request, response) => {
-    try {
-    } catch (error) {
-      console.log(error);
-    }
-  };
+	const availableColours = async (request, response) => {
+		try {
+			const colours = await db.Colour.findAll({ where: { available: true } });
+			const dataToClient = colours.map((colour) => {
+				return {
+					id: colour.id,
+					name: colour.name,
+					code: colour.colourCode,
+				};
+			});
+			response.send(dataToClient);
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
 
-  const add = async (request, response) => {
-    try {
-    } catch (error) {
-      console.log(error);
-    }
-  };
+	const add = async (request, response) => {
+		try {
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  const edit = async (request, response) => {
-    try {
-    } catch (error) {
-      console.log(error);
-    }
-  };
+	const edit = async (request, response) => {
+		try {
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  return {
-    allColours,
-    availableColours,
-    add,
-    edit,
-  };
+	return {
+		allColours,
+		availableColours,
+		add,
+		edit,
+	};
 }
