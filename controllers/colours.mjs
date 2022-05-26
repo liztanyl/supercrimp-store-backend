bcc6d17cb824d8e828655dcff75a430ba7d252a6
gcskhor <gcskhor@yahoo.com.sg>
2022-05-26 22:29:36 +0800

    added allColour

--- controllers/colours.mjs
+++ controllers/colours.mjs
@@ -1,45 +1,56 @@
 export default function initColoursController(db) {
-	const allColours = async (request, response) => {
-		try {
-		} catch (error) {
-			console.log(error);
-		}
-	};
+  const allColours = async (request, response) => {
+    try {
+      const colours = await db.Colour.findAll();
+      const dataToClient = colours.map((colour) => {
+        return {
+          id: colour.id,
+          name: colour.name,
+          code: colour.colourCode,
+          available: colour.available,
+        };
+      });
+      console.log(dataToClient);
+      response.send(dataToClient);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	const availableColours = async (request, response) => {
-		try {
-			const colours = await db.Colour.findAll({ where: { available: true } });
-			const dataToClient = colours.map((colour) => {
-				return {
-					id: colour.id,
-					name: colour.name,
-					code: colour.colourCode,
-				};
-			});
-			response.send(dataToClient);
-		} catch (error) {
-			console.log(error.message);
-		}
-	};
+  const availableColours = async (request, response) => {
+    try {
+      const colours = await db.Colour.findAll({ where: { available: true } });
+      const dataToClient = colours.map((colour) => {
+        return {
+          id: colour.id,
+          name: colour.name,
+          code: colour.colourCode,
+        };
+      });
+      response.send(dataToClient);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
-	const add = async (request, response) => {
-		try {
-		} catch (error) {
-			console.log(error);
-		}
-	};
+  const add = async (request, response) => {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	const edit = async (request, response) => {
-		try {
-		} catch (error) {
-			console.log(error);
-		}
-	};
+  const edit = async (request, response) => {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	return {
-		allColours,
-		availableColours,
-		add,
-		edit,
-	};
+  return {
+    allColours,
+    availableColours,
+    add,
+    edit,
+  };
 }
