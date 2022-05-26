19e1212990e7abbbd88c5e00838d3bc454649b77
gcskhor <gcskhor@yahoo.com.sg>
2022-05-26 14:27:00 +0800

    completed product edit endpoint

--- controllers/products.mjs
+++ controllers/products.mjs
@@ -1,94 +1,101 @@
 const formatProduct = (product) => {
-	const colours = product.colours.map((colour) => {
-		return {
-			id: colour.id,
-			name: colour.name,
-			code: colour.colourCode,
-		};
-	});
+  const colours = product.colours.map((colour) => {
+    return {
+      id: colour.id,
+      name: colour.name,
+      code: colour.colourCode,
+    };
+  });
 
-	return {
-		id: product.id,
-		name: product.name,
-		description: product.description,
-		usualPrice: product.usualPrice,
-		currentPrice: product.currentPrice,
-		available: product.available,
-		colours,
-	};
+  return {
+    id: product.id,
+    name: product.name,
+    description: product.description,
+    usualPrice: product.usualPrice,
+    currentPrice: product.currentPrice,
+    available: product.available,
+    colours,
+  };
 };
 
 export default function initProductsController(db) {
-	const allProducts = async (request, response) => {
-		try {
-			const products = await db.Product.findAll({
-				include: db.Colour,
-			});
+  const allProducts = async (request, response) => {
+    try {
+      const products = await db.Product.findAll({
+        include: db.Colour,
+      });
 
-			const dataToClient = products.map((product) => formatProduct(product));
+      const dataToClient = products.map((product) => formatProduct(product));
 
-			console.log(dataToClient);
-			response.send(dataToClient);
-		} catch (error) {
-			console.log(error.message);
-		}
-	};
+      response.send(dataToClient);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
-	const availableProducts = async (request, response) => {
-		try {
-			const products = await db.Product.findAll({
-				where: { available: true },
-				include: db.Colour,
-			});
+  const availableProducts = async (request, response) => {
+    try {
+      const products = await db.Product.findAll({
+        where: { available: true },
+        include: db.Colour,
+      });
 
-			const dataToClient = products.map((product) => formatProduct(product));
+      const dataToClient = products.map((product) => formatProduct(product));
 
-			console.log(dataToClient);
-			response.send(dataToClient);
-		} catch (error) {
-			console.log(error.message);
-		}
-	};
+      response.send(dataToClient);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
-	const product = async (request, response) => {
-		try {
-			console.log(request.params);
-			const { productId } = request.params;
-			console.log(productId);
-			const product = await db.Product.findOne({
-				where: {
-					id: productId,
-				},
-				include: db.Colour,
-			});
+  const product = async (request, response) => {
+    try {
+      console.log(request.params);
+      const { productId } = request.params;
+      console.log(productId);
+      const product = await db.Product.findOne({
+        where: {
+          id: productId,
+        },
+        include: db.Colour,
+      });
 
-			const dataToClient = formatProduct(product);
+      const dataToClient = formatProduct(product);
 
-			console.log(dataToClient);
-			response.send(dataToClient);
-		} catch (error) {
-			console.log(error.message);
-		}
-	};
+      response.send(dataToClient);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
-	const add = async (request, response) => {
-		try {
-		} catch (error) {
-			console.log(error);
-		}
-	};
+  const add = async (request, response) => {
+    try {
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-	const edit = async (request, response) => {
-		try {
-		} catch (error) {
-			console.log(error);
-		}
-	};
-	return {
-		allProducts,
-		availableProducts,
-		product,
-		add,
-		edit,
-	};
+  const edit = async (request, response) => {
+    try {
+      const { id, name, description, usualPrice, currentPrice } = request.body;
+      const updatedProduct = await db.Product.update(
+        {
+          name,
+          description,
+          currentPrice,
+          usualPrice,
+        },
+        { where: { id } }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return {
+    allProducts,
+    availableProducts,
+    product,
+    add,
+    edit,
+  };
 }
