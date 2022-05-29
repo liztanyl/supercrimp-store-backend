d1527bb682339f58eab9f0416cef823e96923b2e
gcskhor <gcskhor@yahoo.com.sg>
2022-05-29 08:36:45 +0800

    added order pending controller

--- controllers/orders.mjs
+++ controllers/orders.mjs
@@ -1,6 +1,61 @@
+const formatOrder = (order) => {
+  const { user, products } = order;
+  const formattedProducts = products.map((product) => {
+    return {
+      id: product.id,
+      name: product.name,
+      currentPrice: product.currentPrice,
+    };
+  });
+
+  return {
+    id: order.id,
+    totalCost: order.totalCost,
+    deliveryFee: order.deliveryFee,
+    complete: order.complete,
+    createdAt: order.createdAt,
+    products: formattedProducts,
+    user: {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      addressLine1: user.addressLine1,
+      addressLine2: user.addressLine2,
+      postalCode: user.postalCode,
+      phone: user.phone,
+    },
+  };
+};
+
 export default function initOrdersController(db) {
-  const index = async (request, response) => {
+  const pending = async (request, response) => {
     try {
+      const orders = await db.Order.findAll({
+        where: { complete: false },
+        include: [
+          { model: db.User },
+          {
+            model: db.OrderProduct,
+            include: [{ model: db.Product }, { model: db.Colour }],
+          },
+        ],
+      });
+
+      // const dataToClient = orders.map((order) => formatOrder(order));
+      console.log(orders);
+      response.send(orders);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const completed = async (request, response) => {
+    try {
+      // console.log(request.body);
+      // const orders = await db.Order.findAll({
+      //   where: { complete: true },
+      // });
+      // console.log(orders);
     } catch (error) {
       console.log(error);
     }
@@ -14,7 +69,8 @@ export default function initOrdersController(db) {
   };
 
   return {
-    index,
+    pending,
+    completed,
     checkout,
   };
 }
