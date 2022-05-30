bb378c588df29c6632116e8f365de932b904dbcd
gcskhor <gcskhor@yahoo.com.sg>
2022-05-30 18:03:44 +0800

    corrected column headers

--- models/adminUser.mjs
+++ models/adminUser.mjs
@@ -1,43 +1,33 @@
 export default function initAdminUserModel(sequelize, DataTypes) {
-	return sequelize.define(
-		'admin_user',
-		{
-			id: {
-				allowNull: false,
-				autoIncrement: true,
-				primaryKey: true,
-				type: DataTypes.INTEGER,
-			},
-			userId: {
-				type: DataTypes.INTEGER,
-				references: {
-					model: 'users',
-					key: 'id',
-				},
-			},
-			deliveryFee: {
-				type: DataTypes.DECIMAL(10, 2),
-			},
-			totalCost: {
-				allowNull: false,
-				type: DataTypes.DECIMAL(10, 2),
-			},
-			complete: {
-				allowNull: false,
-				type: DataTypes.BOOLEAN,
-			},
-			createdAt: {
-				allowNull: false,
-				type: DataTypes.DATE,
-			},
-			updatedAt: {
-				allowNull: false,
-				type: DataTypes.DATE,
-			},
-		},
-		{
-			tableName: 'admin_users',
-			underscored: true,
-		}
-	);
+  return sequelize.define(
+    "admin_user",
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      tableName: "admin_users",
+      underscored: true,
+    }
+  );
 }
