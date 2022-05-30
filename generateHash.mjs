06081a4861116e9c72fd9eef12d29fc6cb1d971b
gcskhor <gcskhor@yahoo.com.sg>
2022-05-30 17:59:58 +0800

    added adminUser login middleware authenticator

--- generateHash.mjs
+++ generateHash.mjs
@@ -1,8 +1,11 @@
 import jsSHA from "jssha";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const SALT = process.env.SALT;
 
 export default function generateHash(password) {
-  console.log(SALT);
   const shaObj = new jsSHA("SHA-512", "TEXT", { encoding: "UTF8" });
   shaObj.update(SALT + password);
   const hash = shaObj.getHash("HEX");
--- index.mjs
+++ index.mjs
@@ -1,27 +1,34 @@
-import cors from 'cors';
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import methodOverride from 'method-override';
-import dotenv from 'dotenv';
-import bindRoutes from './routes.mjs';
+import cors from "cors";
+import express from "express";
+import cookieParser from "cookie-parser";
+import methodOverride from "method-override";
+import dotenv from "dotenv";
+import bindRoutes from "./routes.mjs";
+
+import initAdminUsersController from "./controllers/adminUsers.mjs";
+import db from "./models/index.mjs";
+const AdminUsersController = initAdminUsersController(db);
 
 dotenv.config();
 
-const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
 
 const app = express();
 app.use(
-	cors({
-		credentials: true,
-		origin: FRONTEND_URL,
-	})
+  cors({
+    credentials: true,
+    origin: FRONTEND_URL,
+  })
 );
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride('_method'));
-app.use(express.static('public'));
+app.use(methodOverride("_method"));
+app.use(express.static("public"));
 app.use(express.json());
+
+// CUSTOM MIDDLEWARE
+app.use(AdminUsersController.checkAuthAdmin);
 
 bindRoutes(app);
 
