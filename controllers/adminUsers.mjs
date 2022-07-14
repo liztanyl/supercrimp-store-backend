fc0327ee6b42cf2f711bca958e39e319316ed0bf
gcskhor <gcskhor@yahoo.com.sg>
2022-07-14 22:20:37 +0800

    commented out lines

--- controllers/adminUsers.mjs
+++ controllers/adminUsers.mjs
@@ -19,6 +19,12 @@ export default function initAdminUsersController(db) {
       } else {
         const id = adminUser.id;
         const token = jwt.sign({ id, role: 'admin' }, JWT_SECRET_KEY);
+        // response.header('Access-Control-Allow-Credentials', true);
+        // response.header('Access-Control-Allow-Credentials', true);
+        // response.header(
+        //   'Access-Control-Allow-Headers',
+        //   'Origin, X-Requested-With, Content-Type, Accept'
+        // );
         response.cookie('token', token);
         response.send({ token: token });
       }
