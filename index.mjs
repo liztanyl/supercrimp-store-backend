b55cb640841b20ca5e6c2b812f63d53a5468e2d5
gcskhor <gcskhor@yahoo.com.sg>
2022-07-14 23:54:22 +0800

    removed trust proxy to cors middleware

--- index.mjs
+++ index.mjs
@@ -1,17 +1,17 @@
-import cors from "cors";
-import express from "express";
-import cookieParser from "cookie-parser";
-import methodOverride from "method-override";
-import dotenv from "dotenv";
-import bindRoutes from "./routes.mjs";
+import cors from 'cors';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import methodOverride from 'method-override';
+import dotenv from 'dotenv';
+import bindRoutes from './routes.mjs';
 
-import initAdminUsersController from "./controllers/adminUsers.mjs";
-import db from "./models/index.mjs";
+import initAdminUsersController from './controllers/adminUsers.mjs';
+import db from './models/index.mjs';
 const AdminUsersController = initAdminUsersController(db);
 
 dotenv.config();
 
-const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
 
 const app = express();
 app.use(
@@ -20,11 +20,12 @@ app.use(
     origin: FRONTEND_URL,
   })
 );
-app.set("view engine", "ejs");
+// app.set("trust proxy", 1)
+app.set('view engine', 'ejs');
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride("_method"));
-app.use(express.static("public"));
+app.use(methodOverride('_method'));
+app.use(express.static('public'));
 app.use(express.json());
 
 // CUSTOM MIDDLEWARE
